dc7b7ad0a9b120d3c1ce8c11e245a31b79cadc41
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-08 16:17:55 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,18 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by app-kaihatsusha on 08/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
